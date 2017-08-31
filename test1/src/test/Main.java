b748c99612db6e274966121b7d78743df2acbb03
wenlf <515302125@qq.com>
2017-08-31 19:54:42 +0800

    e

--- test1/src/test/Main.java
+++ test1/src/test/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("e");
 		System.out.println("ef");
+		System.out.println("fg");
 	}
 
 }
---
+++ www/src/www/GG.java
@@ -0,0 +1,10 @@
+package www;
+
+public class GG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
